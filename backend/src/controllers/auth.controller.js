c89dd071e03d5df0d7c62a8cf19c6c0ff14833af
unknown <sahilbakshi3@example.com>
2025-05-11 05:22:30 +0530

    added login and lodout endpoints

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -58,9 +58,39 @@ export async function signup(req, res) {
 }
 
 export async function login(req, res) {
-    res.send("login Route");
+    try {
+        const { email, password } = req.body;
+
+        if(!email || !password){
+            return res.status(400).json({ message: "All Fields are required" });
+        }
+
+        const user = await User.findOne({ email });
+        if(!user) return res.status(401).json({ message: "Invalid email or Password" });
+
+        const isPasswordCorrect = await user.matchPassword(password);
+        if(!isPasswordCorrect) return res.status(401).json({ message: "Invalid email or Password" });
+
+        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET_KEY, {
+            expiresIn: "7d",
+        })
+
+        res.cookie("jwt", token, {
+            maxAge: 7 * 24 * 60 * 60 * 100,
+            httpOnly: true,
+            sameSite: "strict",
+            secure: process.env.NODE_ENV === "production"
+        })
+
+        res.status(200).json({ success: true, user });
+
+    } catch (error) {
+        console.log("Error in login controller", error.message );
+        res.status(500).json({ success: true, user });
+    }
 }
 
 export async function logout(req, res) {
-    res.send("logout Route");
+    res.clearCookie("jwt");
+    res.status(500).json({ success: true, message: "Logout successful" });
 }
